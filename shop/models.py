abb6a6f4aab42dcf5fe4d3b0931af8f7c5d6e902
modonohoe <meganodt@gmail.com>
2023-12-12 23:29:09 +0000

    Feat(shop): add models for store to models.py

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from accounts.models import CustomUser
+from cloudinary.models import CloudinaryField
+import datetime
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.CharField(
+        max_length=300, default='', blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
+    image_alt_text = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.customer}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name}"
