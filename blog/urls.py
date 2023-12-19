642e63b73a30aff998c544cb5fba1ce9a153ed37
modonohoe <meganodt@gmail.com>
2023-12-19 02:32:31 +0000

    Feat(blog): add like function to posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('blogposts/', views.PostList.as_view(), name='blogposts'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from .forms import CommentForm
 from django.views import generic, View
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.shortcuts import render
 from django.db.models import Count
+from django.http import HttpResponseRedirect
 from .models import Post
 
 
@@ -68,3 +69,14 @@ class PostDetail(View):
             },
         )
 
+
+class PostLike(View):
+
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
