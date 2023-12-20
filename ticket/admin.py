81bbc6b410cb140f27b5c76cacee38b09a53c1ce
modonohoe <meganodt@gmail.com>
2023-12-20 21:35:26 +0000

    Feat(ticket): register Admin ticket site

--- ticket/admin.py
+++ ticket/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import EnquiryTicket
 
-# Register your models here.
+
+class EnquiryTicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'country',
+                    'completion_date', 'budget', 'status')
+    list_filter = ('status', 'country')
+    search_fields = ('title', 'description', 'user__username')
+    date_hierarchy = 'completion_date'
+
+
+admin.site.register(EnquiryTicket, EnquiryTicketAdmin)
