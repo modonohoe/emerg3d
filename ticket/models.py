a5a119db865eb7d7bed0a57f0c3ecca3d719c73f
modonohoe <>
2023-12-23 05:37:55 +0000

    Fix: datetime comparison issue in Ticket module

--- ticket/models.py
+++ ticket/models.py
@@ -24,7 +24,7 @@ class EnquiryTicket(models.Model):
     title = models.CharField(max_length=100, help_text='Your project or item name')
     description = models.TextField(max_length=400, help_text='What do you want printed and what dimensions, colors, finishes etc.')
     country = models.CharField(max_length=2, choices=COUNTRY_CHOICES, default='IE')
-    completion_date = models.DateTimeField(
+    completion_date = models.DateField(
         default=date.today, help_text='If not time-sensitive please submit 12/12/2024', validators=[validate_not_in_past])
     budget = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, help_text='€ amount (optional)', validators=[MinValueValidator(0)])
     status = models.CharField(max_length=8, choices=STATUS_CHOICES, default='pending')
